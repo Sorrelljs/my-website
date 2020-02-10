1659a4b132bec2622264d38cadf96ddaa3497ca9
Sorrelljs <[sjsohota@gmail.com]>
2020-02-09 21:05:12 -0800

    jquery

--- js/index.js
+++ js/index.js
@@ -1,6 +1,29 @@
-$(document).ready()
-
-document.getElementById("btn-icon").addEventListener("Click")
-
+$(document).ready(function () {
+    // do jQuery
 
 
+
+
+    var $test = $(".test123").hide();
+
+
+
+    $(".container-1441").mouseenter(
+        function () {
+            $test.fadeIn(450);
+        }
+    );
+
+
+
+    // var $btn = $('.divbutton').hide()
+    // $(".mybooks").mouseenter(function () {
+    //     $btn.show();
+    // });
+
+    $(".container-1441").mouseleave(function () {
+        $test.fadeOut(250);
+    });
+
+
+})
