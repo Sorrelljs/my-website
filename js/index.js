e136c167e4caf5a896d6b19b4d73a82e932715f4
Sorrelljs <[sjsohota@gmail.com]>
2020-02-15 13:32:14 -0800

    jquery updates

--- js/index.js
+++ js/index.js
@@ -9,6 +9,8 @@ $(document).ready(function () {
     const $inhabitent = $(".test-inhabitent").hide();
     const $pong = $(".test-pong").hide();
 
+    const $icon = $("#icon2020");
+
 
     // BACKGROUND TAGS
 
@@ -80,9 +82,43 @@ $(document).ready(function () {
 
     });
 
-    $(".work-text-button").hover(function () {
-        $(this).toggleClass('.forum_hover')
+    $(".work-display-button").mouseenter(function () {
+        $($icon).toggleClass('flip');
     });
+
+    $(".work-display-button").mouseleave(function () {
+        $($icon).removeClass('flip');
+    });
+
+    $(".work-display-button").mouseenter(function () {
+        $(this).css('border', 'none');
+    });
+
+    $(".work-display-button").mouseleave(function () {
+        $(this).css('border', '3px solid white');
+    });
+
+
+
+
+
+    function animatethis(targetElement, speed) {
+        $(targetElement).animate({ marginLeft: "+=750px" },
+            {
+                duration: speed,
+                complete: function () {
+                    targetElement.animate({ marginLeft: "-=750px" },
+                        {
+                            duration: speed,
+                            complete: function () {
+                                animatethis(targetElement, speed);
+                            }
+                        });
+                }
+            });
+    };
+
+    animatethis($('.gulplogo'), 5000);
 
 
 });
