733836813887484a40483e5bb39bbc5ffbd16756
Sorrelljs <[sjsohota@gmail.com]>
2020-02-14 16:57:51 -0800

    updated jquery animation

--- js/index.js
+++ js/index.js
@@ -4,26 +4,70 @@ $(document).ready(function () {
 
 
 
-    var $test = $(".test123").hide();
+    const $quote = $(".test-quotes").hide();
+    const $insta = $(".test-instanews").hide();
+    const $inhabitent = $(".test-inhabitent").hide();
+    const $pong = $(".test-pong").hide();
+
+
+    // BACKGROUND TAGS
 
 
 
-    $(".container-1441").mouseenter(
+
+
+    $(".quote-tag").mouseenter(
         function () {
-            $test.fadeIn(450);
+            $quote.fadeIn(450);
         }
     );
 
-
-
-    // var $btn = $('.divbutton').hide()
-    // $(".mybooks").mouseenter(function () {
-    //     $btn.show();
-    // });
-
-    $(".container-1441").mouseleave(function () {
-        $test.fadeOut(250);
+    $(".quote-tag").mouseleave(function () {
+        $quote.fadeOut(250);
     });
 
 
-})
+    // PONG TAG
+
+    $(".pong-tag").mouseenter(
+        function () {
+            $pong.fadeIn(450);
+        }
+    );
+
+    $(".pong-tag").mouseleave(function () {
+        $pong.fadeOut(250);
+    });
+
+    // INHABITENT 
+
+    $(".inhabitent-tag").mouseenter(
+        function () {
+            $inhabitent.fadeIn(450);
+        }
+    );
+
+    $(".inhabitent-tag").mouseleave(function () {
+        $inhabitent.fadeOut(250);
+    });
+
+
+    // INSTA NEWS
+
+
+    $(".insta-tag").mouseenter(
+        function () {
+            $insta.fadeIn(450);
+        }
+    );
+
+    $(".insta-tag").mouseleave(function () {
+        $insta.fadeOut(250);
+    });
+
+    $(".work-text-button").hover(function () {
+        $(this).toggleClass('.forum_hover')
+    });
+
+
+});
